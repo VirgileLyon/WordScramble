6b759cec75f2fec7080b92a08c4e19ae058bfce7
VirgileLyon <45287046+VirgileLyon@users.noreply.github.com>
2021-04-21 13:15:13 +0200

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Virgile Vercellis on 21/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
